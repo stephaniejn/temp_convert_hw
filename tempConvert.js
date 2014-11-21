f944c4274cb29272feaac7a46d497eba17299b33
stephaniejn <stephaniejnilles@gmail.com>
2014-11-21 11:27:16 -0800

    Updated Clear Function

--- tempConvert.js
+++ tempConvert.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(){
-var button = document.querySelector('#Convert');
+	var button = document.querySelector('#Convert');
 	button.addEventListener("click", function(){
-	var input = document.getElementById("temp").value;
-	var Celsius = Math.round((input - 32) / 1.8) ;
-	console.log(Celsius);
-	document.getElementById('result').innerHTML = Celsius;
+		var input = document.getElementById("temp").value;
+		var Celsius = Math.round((input - 32) / 1.8) ;
+		console.log(Celsius);
+		document.getElementById('result').innerHTML = Celsius;
 	})
 }
 )
 document.addEventListener("DOMContentLoaded", function(){
-var clear = document.querySelector('#Clear');
+	var clear = document.querySelector('#Clear');
 	clear.addEventListener("click", function(){
-	var input = " "
-	console.log(input);
-	document.getElementById('result').innerHTML = input;
+		var input = " "
+		console.log(input);
+		document.getElementById('result').innerHTML = input;
+		document.getElementById('temp').value = input;
 	})
 }
 )
