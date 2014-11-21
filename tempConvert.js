653d97c90ddba1660958223721cb724995246b5a
stephaniejn <stephaniejnilles@gmail.com>
2014-11-21 12:28:11 -0800

    Updated with red/blue formatting

--- tempConvert.js
+++ tempConvert.js
@@ -3,11 +3,23 @@ document.addEventListener("DOMContentLoaded", function(){
 	button.addEventListener("click", function(){
 		var input = document.getElementById("temp").value;
 		var Celsius = Math.round((input - 32) / 1.8) ;
-		console.log(Celsius);
-		document.getElementById('result').innerHTML = Celsius;
-	})
-}
-)
+		console.log("celsius", Celsius)
+		document.getElementById('result').innerHTML = Celsius
+		if (Celsius > 0) {
+			var resultObj=document.getElementById("result");
+			resultObj.style.backgroundColor="#880000";
+			resultObj.style.color="white";
+		}
+		else{
+			var resultObj2=document.getElementById("result");
+			resultObj2.style.backgroundColor="blue"
+			resultObj2.style.color="white";
+		}
+		;
+	});	
+
+});
+
 document.addEventListener("DOMContentLoaded", function(){
 	var clear = document.querySelector('#Clear');
 	clear.addEventListener("click", function(){
